0ecd923461cf26766ef756c3f76bf43eee2822dc
Daniel Roe <daniel@roe.dev>
2022-07-16 17:38:40 +0100

    chore: add coverage config for vite

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      'package-name': fileURLToPath(
+        new URL('./src/index.ts', import.meta.url).href
+      ),
+    },
+  },
+  test: {
+    coverage: {
+      include: ['src'],
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+})
