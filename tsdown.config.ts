c4c3a4563e5a488f629abcdc66ee9bc98bd9d679
Daniel Roe <daniel@roe.dev>
2026-09-08 11:57:15 +0100

    chore: use tsdown to test publint + attw on build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   dts: { oxc: true },
   exports: { devExports: true },
+  publint: true,
+  attw: {
+    profile: 'esm-only',
+    level: 'error',
+  },
 })
